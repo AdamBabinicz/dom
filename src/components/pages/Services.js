b86f6f59c153443906f008f8cdb1e2db45e8836a
hipek <puaro@vp.pl>
2020-11-14 17:45:04 +0100

    zmiany w tekście

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -6,7 +6,7 @@ export default function Services() {
     <div className="services">
       <p>
         Ania, Adaś i Marysia (Dzidka). Sądząc po odświętnych strojach, zdjęcie
-        było zrobione w niedzielę.
+        zostało zrobione w niedzielę.
       </p>
       <h1>Dzieci</h1>
     </div>
