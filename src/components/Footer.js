595c0a38dfdc83a4fde08490aca94686b01dda69
hipek <puaro@vp.pl>
2020-11-16 19:07:10 +0100

    zmiana

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import styled, { css } from "styled-components";
 // import BackgroundImg from "../images/key.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
